d0ba411484137f4be97322852d5a06f365cff536
xilxilx <ikylioss@gmail.com>
2024-09-09 01:57:01 +0300

    first

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const { defineConfig } = require("cypress")
+module.exports = defineConfig({
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
